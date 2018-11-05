524988e4f261f79afc39363bcd624e2b21c5db0f
harryphelps2 <harryphelps1@gmail.com>
2018-11-05 09:02:07 +0000

    Add delivery details step to checkout.

--- shoepersonic2/checkout/forms.py
+++ shoepersonic2/checkout/forms.py
@@ -12,3 +12,8 @@ class DeliveryForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('first_name', 'last_name', 'address_line_1', 'address_line_2', 'address_line_3', 'town_or_city', 'county', 'postcode')
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('email', 'running_club','first_name', 'last_name', 'address_line_1', 'address_line_2', 'address_line_3', 'town_or_city', 'county', 'postcode')
--- shoepersonic2/checkout/views.py
+++ shoepersonic2/checkout/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib.auth.decorators import login_required
-from .forms import ContactDetailsForm
+from .forms import ContactDetailsForm, DeliveryForm, OrderForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.utils import timezone
@@ -14,21 +14,19 @@ stripe.api_key = settings.STRIPE_SECRET
 
 def contact_details(request):
     """Collect email and running club and add to session"""
-    # If user is authenticated take the email address from
     user = request.user
     if user.is_authenticated:
-        # get user object and populate contact form with email and running club
-    # user and running club from profile
         contact_details = {'email': user.email, 'running_club': user.profile.running_club }
-        contact_form = ContactDetailsForm(contact_details)
-    # If the form is valid 
         if request.method == "POST":
             if contact_form.is_valid():
                 request.session['email'] = contact_form.cleaned_data['email']
                 request.session['running_club'] = contact_form.cleaned_data['running_club']
                 return redirect(reverse('delivery_details'))
     else:
-        contact_details = {'email' : request.session.get('email', None), 'running_club' : request.session.get('running_club', None)}
+        contact_details = {
+            'email' : request.session.get('email', None), 
+            'running_club' : request.session.get('running_club', None)
+            }
         contact_form = ContactDetailsForm(request.POST or contact_details)
         if request.method == "POST":
             print(contact_form)
@@ -37,14 +35,58 @@ def contact_details(request):
                 request.session['running_club'] = contact_form.cleaned_data['running_club']
                 return redirect(reverse('delivery_details'))
     return render(request, 'contact_details.html', {'contact_form': contact_form})
-    # If not get it from the session or from a blank form
-
-    # Add the email to the session and go to step 2
 
 
 def delivery_details(request):
     """Collect name and address and add to session"""
-    return render(request, 'delivery_details.html')
+    user = request.user
+    if user.is_authenticated:
+        delivery_details = {
+            'first_name': user.first_name, 
+            'last_name': user.last_name,
+            'address_line_1': user.profile.address_line_1,
+            'address_line_2': user.profile.address_line_2,
+            'address_line_3 ': user.profile.address_line_3,
+            'town_or_city': user.profile.town_or_city,
+            'county': user.profile.county,
+            'postcode': user.profile.postcode
+             }
+        delivery_form = DeliveryForm(request.POST or delivery_details)
+        if request.method == "POST":
+            if delivery_form.is_valid():
+                request.session['first_name'] = delivery_form.cleaned_data['first_name']
+                request.session['last_name'] = delivery_form.cleaned_data['last_name']
+                request.session['address_line_1'] = delivery_form.cleaned_data['address_line_1']
+                request.session['address_line_2'] = delivery_form.cleaned_data['address_line_2']
+                request.session['address_line_3'] = delivery_form.cleaned_data['address_line_3']
+                request.session['town_or_city'] = delivery_form.cleaned_data['town_or_city']
+                request.session['county'] = delivery_form.cleaned_data['county']
+                request.session['postcode'] = delivery_form.cleaned_data['postcode']
+                return redirect(reverse('submit_order'))
+    else:
+        delivery_details = {
+            'first_name' : request.session.get('first_name', None), 
+            'last_name' : request.session.get('last_name', None),
+            'address_line_1' : request.session.get('address_line_1', None),
+            'address_line_2' : request.session.get('address_line_2', None), 
+            'address_line_3' : request.session.get('address_line_3', None), 
+            'town_or_city' : request.session.get('town_or_city', None), 
+            'county' : request.session.get('county', None), 
+            'postcode' : request.session.get('postcode', None)    
+            }
+        delivery_form = DeliveryForm(request.POST or delivery_details)
+        if request.method == "POST":
+            if delivery_form.is_valid():
+                request.session['first_name'] = delivery_form.cleaned_data['first_name']
+                request.session['last_name'] = delivery_form.cleaned_data['last_name']
+                request.session['address_line_1'] = delivery_form.cleaned_data['address_line_1']
+                request.session['address_line_2'] = delivery_form.cleaned_data['address_line_2']
+                request.session['address_line_3'] = delivery_form.cleaned_data['address_line_3']
+                request.session['town_or_city'] = delivery_form.cleaned_data['town_or_city']
+                request.session['county'] = delivery_form.cleaned_data['county']
+                request.session['postcode'] = delivery_form.cleaned_data['postcode']
+                return redirect(reverse('submit_order'))
+    return render(request, 'delivery_details.html', {'delivery_form': delivery_form})
 
 def submit_order(request):
     """Make payment"""
