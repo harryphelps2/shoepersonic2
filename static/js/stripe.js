5bae72a7cdd3082144d269e9ceecf572473c584a
harryphelps2 <harryphelps1@gmail.com>
2018-11-18 18:04:12 +0000

    Replace stripe key.

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,5 +1,5 @@
 //Publishable key
-var stripe = Stripe('pk_test_oGzjqmtiyqFycxaJ7nO1aTgL');
+var stripe = Stripe('pk_live_urIIwRtSd5m2hbmLgW4uf9Y2');
 var elements = stripe.elements();
 
 // Custom styling can be passed to options when creating an Element.
