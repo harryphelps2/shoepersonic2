8c5d9bc37207db512bb2b5ff99df4e2be3bff3a2
harryphelps2 <harryphelps1@gmail.com>
2018-11-18 14:45:16 +0000

    Add host names.

--- shoepersonic2/settings.py
+++ shoepersonic2/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['shoepersonic2.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['shoepersonic2.herokuapp.com', '127.0.0.1', 'www.shoepersonic.com', 'shoepersonic.com']
 
 
 # Application definition
