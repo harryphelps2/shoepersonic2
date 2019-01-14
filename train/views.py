0b572a22d7c0188538d5b6ba0acc059f1df50197
harryphelps2 <harryphelps1@gmail.com>
2019-01-14 08:16:51 +0000

    Boiler plate testing for weekly volume function.

--- train/views.py
+++ train/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# 
+def decide_weekly_running_volume():
+    """
+    Function that takes customer's choosen experience level and returns a running volume
+    """ 
+    print("hi")
+    return "Hi there"
