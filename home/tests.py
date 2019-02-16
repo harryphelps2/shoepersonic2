f059d57f4cfd5eff29fe36b0fb7833a90710f596
harryphelps2 <harryphelps1@gmail.com>
2019-02-16 10:59:36 +0000

    First homepage test.

--- home/tests.py
+++ home/tests.py
@@ -1,10 +1,15 @@
 from django.test import TestCase, Client
 
-class HomeTests(TestCase):
-    """
-    Tests for home page.
-    """
-    def test_home_page_happy_path(self):
-        client = Client()
-        response = client.get('/')
-        self.assertEqual(response.status_code, 200)
+class SmokeTest(TestCase):
+
+    def test_bad_maths(self):
+        self.assertEqual(1+1, 3)
+
+# class HomeTests(TestCase):
+#     """
+#     Tests for home page.
+#     """
+#     def test_home_page_happy_path(self):
+#         client = Client()
+#         response = client.get('/')
+#         self.assertEqual(response.status_code, 200)
